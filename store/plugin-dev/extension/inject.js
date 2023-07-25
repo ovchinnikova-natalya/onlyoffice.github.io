96ec0ed7ccf4432188920a9dc9d6ab9e80e9081b
Natalia <ovchinnikova.natalya@onlyoffice.com>
2023-07-25 12:47:40 +0300

    test

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "https://ovchinnikova-natalya.github.io/onlyoffice.github.io/sdkjs-plugins/content/plugin-contextmenu_event/";
+	var URL_TO_PLUGIN = "https://ovchinnikova-natalya.github.io/onlyoffice.github.io/sdkjs-plugins/content/helloworld/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
